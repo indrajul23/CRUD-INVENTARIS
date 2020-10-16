eef890d69b213d007caeae7e48bf7df757c1d0cc
indrajuliansh <20170810033@uniku.ac.id>
2020-10-16 09:51:06 +0700

    langkah 4 dari santri koding

--- crud-20/app/Controllers/Post.php
+++ crud-20/app/Controllers/Post.php
@@ -23,4 +23,61 @@ class Post extends Controller
 
         return view('post-index', $data);
     }
+     /**
+     * create function
+     */
+    public function create()
+    {
+        return view('post-create');
+    }
+
+    /**
+     * store function
+     */
+    public function store()
+    {
+        //load helper form and URL
+        helper(['form', 'url']);
+         
+        //define validation
+        $validation = $this->validate([
+            'title' => [
+                'rules'  => 'required',
+                'errors' => [
+                    'required' => 'Masukkan Judul Post.'
+                ]
+            ],
+            'content'    => [
+                'rules'  => 'required',
+                'errors' => [
+                    'required' => 'Masukkan konten Post.'
+                ]
+            ],
+        ]);
+
+        if(!$validation) {
+
+            //render view with error validation message
+            return view('post-create', [
+                'validation' => $this->validator
+            ]);
+
+        } else {
+
+            //model initialize
+            $postModel = new PostModel();
+            
+            //insert data into database
+            $postModel->insert([
+                'title'   => $this->request->getPost('title'),
+                'content' => $this->request->getPost('content'),
+            ]);
+
+            //flash message
+            session()->setFlashdata('message', 'Post Berhasil Disimpan');
+
+            return redirect()->to(base_url('post'));
+        }
+
+    }
 }
